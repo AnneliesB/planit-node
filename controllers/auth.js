af77d5250934c7fd16a9dccf668f7f9bd36f51ae
Annelies B <annelies_bellon@hotmail.com>
2019-10-26 20:15:15 +0200

    add secret key to heroku env

--- controllers/auth.js
+++ controllers/auth.js
@@ -18,7 +18,7 @@ const signup = async (req, res, next) => {
             let token = jwt.sign({
                 uid: result._id,
                 username: result.username
-            }, config.get('jwt.secret'))
+            }, process.env.secretkey || config.get('jwt.secret'))
 
             res.json({
                 "status": "success",
@@ -46,7 +46,7 @@ const login = async (req, res, next) => {
             let token = jwt.sign({
                 uid: result.user._id,
                 username: result.user.username
-            }, config.get('jwt.secret'))
+            }, process.env.secretkey || config.get('jwt.secret'))
             
             return res.json({
                 "status": "success",
