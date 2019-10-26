4c193843d65df18147c1aff7b5777c255c173710
Annelies B <annelies_bellon@hotmail.com>
2019-10-26 20:22:54 +0200

    remove passport authenticate from index route"

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(cors());
-app.use('/', passport.authenticate('jwt', { session: false }), indexRouter);
+app.use('/', indexRouter);
 app.use('/users', usersRouter);
 //app.use('/todos', passport.authenticate('jwt', { session: false }), todosRouter);
 
