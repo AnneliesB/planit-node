e5ec30935f2d4565db09d06c6c5588e6f0ea6be6
Annelies B <annelies_bellon@hotmail.com>
2019-10-26 16:29:26 +0200

    add todos route back

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-//app.use('/todos', passport.authenticate('jwt', { session: false }), todosRouter);
+app.use('/todos', passport.authenticate('jwt', { session: false }), todosRouter);
 
 // catch 404 and forward to error handler
 /* app.use(function(req, res, next) {
